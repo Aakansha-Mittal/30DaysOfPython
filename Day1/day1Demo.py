46f2a0f0d7b68a3661bbba064e90724a77203f3d
Aakansha Mittal <aakansha.mittal@publicissapient.com>
2026-05-18 08:49:15 +0530

    Day1

---
+++ Day1/day1Demo.py
@@ -0,0 +1,41 @@
+#python --version
+
+#Console python - python , exit()
+
+#Arithmetic operations
+print(3 + 2)
+print(3.0+2)
+print(3.0 + 2.0)
+
+print(3 - 2)
+print(3.0-2)
+print(3.0 - 2.0)
+
+print(3 * 2)
+print(3.0*2)
+print(3.0 * 2.0)
+
+print(3 / 2)
+print(3.0/2)
+print(3.0 / 2.0)
+
+print(3 // 2)
+print(3.0//2)
+print(3.0 // 2.0)
+
+print(3 % 2)
+print(3.0%2)
+print(3.0 % 2.0)
+
+#Checking Dat Types
+print(type(10))
+print(type('10'))
+print(type(1.234))
+print(type(1+3j))
+print(type('Aakansha'))
+print(type("Aakansha"))
+print(type('''Aakansha'''))
+print(type([1,2,'a']))
+print(type('a'))
+print(type((1,2,2,'a')))
+print(type({1,}))
---
+++ Day1/day1Excercise.py
@@ -0,0 +1,46 @@
+import sys
+
+#Python Version Que 1
+print(sys.version)
+
+#Que 2
+print(3 + 4)
+print(3.0 + 4)
+print(3.0 + 4.0)
+
+print(3 - 4)
+print(3.0 - 4)
+print(3.0 - 4.0)
+
+print(3 * 4)
+print(3.0 * 4)
+print(3.0 * 4.0)
+
+print(3 / 4)
+print(3.0 / 4)
+print(3.0 / 4.0)
+
+print(3 // 4)
+print(3.0 // 4)
+print(3.0 // 4.0)
+
+print(3 % 4)
+print(3.0 % 4)
+print(3.0 % 4.0)
+
+#Que 3
+print('Aakansha')
+print("Mittal")
+print('India')
+print('I am enjoying 30 days of python')
+
+#Que 4
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type('Aakansha'))
+print(type('Mittal'))
+print(type('India'))
+
