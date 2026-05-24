6880f97f00d45e8cb05a5430bc5a8fbda4e1d7be
Aakansha Mittal <aakansha.mittal@publicissapient.com>
2026-05-24 11:35:08 +0530

    Day3 some excercide

---
+++ Day3/day3Excercise.py
@@ -0,0 +1,27 @@
+age = 22
+height = 144.5
+complex = 4+2j
+print(complex)
+b = int(input("Enter base of triangle"))
+h = int(input("Enter height of triangle"))
+area = 0.5*b*h
+print("Area of triangle - ", area)
+
+a = int(input("Enter side a : "))
+b = int(input("Enter side b : "))
+c = int(input("Enter side c : "))
+perimeter = a+b+c
+print(perimeter)
+
+l = int(input("Enter length of rectangle : "))
+b = int(input("Enter width of rectangle : "))
+area_rect = l*b
+peri_rect = 2*(l+b)
+print("Ared rectabgle : ", area_rect)
+print("Peri rect : ", peri_rect)
+
+
+
+
+
+
